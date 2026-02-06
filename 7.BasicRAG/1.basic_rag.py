7ae5a6c83c47f53400ae04c8cc9fa988579f0de8
Omar Mohamed <engomar.mohamed7@gmail.com>
2026-02-06 22:08:18 +0200

    basic_rag_pipeline

---
+++ 7.BasicRAG/1.basic_rag.py
@@ -0,0 +1,56 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
+import os
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
+
+# Step1 : load env variables
+load_dotenv()
+
+# Step2 : load the documents
+loader = TextLoader("docs.txt")
+documents = loader.load()
+
+# Step3 : split the text into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = text_splitter.split_documents(documents)
+
+# Step4 : create the embeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+vectorstore = FAISS.from_documents(chunks, embeddings)
+
+# Step5 : create the retrieval pipeline
+retriever = vectorstore.as_retriever()
+
+# Step6: Initialize the model and prompt
+model = ChatOllama(model="phi4-mini:latest")
+
+prompt = ChatPromptTemplate.from_template(
+    """
+Answer the question "{input}" based solely on the context below:
+<context>
+{context}
+</context>
+If you can't find an answer, say "I don't know." 
+"""
+)
+
+# TODO: Enhance these and understand what is going and why there is two chains one for documents and one for retrieval
+
+# Step7: Create the document chain
+document_chain = create_stuff_documents_chain(model, prompt)
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+
+# Step8: Query the chain
+query = (
+    "what are the key takeways from the document?"  # Replace with your actual question
+)
+response = retrieval_chain.invoke({"input": query, "context": retriever})
+
+print(response)
